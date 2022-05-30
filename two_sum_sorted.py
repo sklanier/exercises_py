1b8ef9abd72aadd8c25875c3037417ad41365746
sklanier <stevekentlanier@gmail.com>
2022-05-30 16:12:51 -0500

    working commit

--- two_sum_sorted.py
+++ two_sum_sorted.py
@@ -1,2 +1,14 @@
-def twoSumSorted(arr: List[int], target: int) -> List[int]:
-    pass
+"""
+TESTS
+
+t1 = [2,3,4,5,8,11,18], 8
+
+
+
+"""
+t1 = [2,3,4,5,8,11,18], 8
+
+def twoSumSorted(arr, target):
+    p1, p2 = arr[0], arr[-1]
+
+print(t1)
