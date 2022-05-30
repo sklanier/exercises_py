89711c75ea799e64128e15a15bc285ee3a5a54fb
sklanier <stevekentlanier@gmail.com>
2022-05-30 16:26:32 -0500

    two-sum-sorted complete

--- two_sum_sorted.py
+++ two_sum_sorted.py
@@ -6,9 +6,22 @@ t1 = [2,3,4,5,8,11,18], 8
 
 
 """
-t1 = [2,3,4,5,8,11,18], 8
+t1 = [
+    [2,3,4,5,8,11,18],
+    8
+]
 
 def twoSumSorted(arr, target):
-    p1, p2 = arr[0], arr[-1]
+    l, r = 0, len(arr) - 1
 
-print(t1)
+    while l < r:
+        s = arr[l] + arr[r]
+        if s == target:
+            return [l, r]
+        if s < target:
+            l += 1
+        else:
+            r -= 1
+        
+
+print(twoSumSorted(t1[0], t1[1]))
